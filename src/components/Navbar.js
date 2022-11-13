30ae5ec9ef167f4e1accf3f1202b98b904856e07
kgadiletofficial <kgadiletofficial@gmail.com>
2022-11-13 17:02:14 +0600

    add2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import {Button, Grid} from "@material-ui/core";
 import {NavLink} from "react-router-dom";
-// import {LOGIN_ROUTE} from "../../../firebase_react_messanger/src/utils/consts";
-// import {Context} from "../../../firebase_react_messanger/src";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {LOGIN_ROUTE} from '../utils/consts'
 import {Context} from "../index";
